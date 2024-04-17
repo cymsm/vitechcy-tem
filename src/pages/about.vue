02541dd3711bf62089370b4e7b748e90e4a66668
cy <756216685@qq.com>
2024-04-17 14:18:06 +0800

    unplugin-vue-router自动路由

---
+++ src/pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>about</div>
+  <RouterLink to="/">home</RouterLink>
+  <RouterLink to="/about">about</RouterLink>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<style scoped></style>
